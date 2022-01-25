711a0fa353e3eccb821f18e0e25d58b152b6e93d
Tim Dugan <tadugan@me.com>
2022-01-25 15:10:07 -0600

    todo-item-view component displays json of selected task

--- src/app/todos/todo-item-view/todo-item-view.component.ts
+++ src/app/todos/todo-item-view/todo-item-view.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item-view',
   templateUrl: './todo-item-view.component.html',
   styleUrls: ['./todo-item-view.component.css']
 })
-export class TodoItemViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class TodoItemViewComponent{
+  @Input() selectedTask: object = {};
 
 }
